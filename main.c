2adba54b8d902d120d1d4bea0155e55fe6e4a6fc
domanttas <domantasgiedraitis@gmail.com>
2019-02-09 19:02:58 +0200

    Upgraded chat code for client

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ int main() {
 
     //Number of bytes
     int nBytes;
+    int sBytes;
 
     //Address info in struct
     struct addrinfo hints, *serverinfo, *p;
@@ -19,8 +20,14 @@ int main() {
 
     //Data
     char buffer[MAXDATASIZE];
+    char sendBuffer[MAXDATASIZE];
 
     char s[INET6_ADDRSTRLEN];
+
+    int messageLength;
+
+    //Descriptors
+    fd_set read_fds;
 
     //Cleaning
     memset(&hints, 0, sizeof hints);
@@ -28,20 +35,20 @@ int main() {
     hints.ai_socktype = SOCK_STREAM;
 
     if ((rv = getaddrinfo("localhost", PORT, &hints, &serverinfo)) != 0) {
-        fprintf(stderr, "Get address info: %s\n", gai_strerror(rv));
+        printf("Get address info: %s\n", gai_strerror(rv));
         exit(1);
     }
 
     //Looping through results and connecting to server
     for (p = serverinfo; p != NULL; p = p -> ai_next) {
         if ((sockfd = socket(p -> ai_family, p -> ai_socktype, p -> ai_protocol)) == -1) {
-            perror("Client error while connecting");
+            printf("Client error while connecting");
             continue;
         }
 
         if (connect(sockfd, p -> ai_addr, p -> ai_addrlen) == -1) {
             close(sockfd);
-            perror("Client error while connecting");
+            printf("Client error while connecting");
             continue;
         }
 
@@ -50,7 +57,7 @@ int main() {
 
     //If connection failed
     if (p == NULL) {
-        fprintf(stderr, "Client failed to connect to host\n");
+        printf(stderr, "Client failed to connect to host\n");
         exit(1);
     }
 
@@ -61,16 +68,42 @@ int main() {
 
     //Done, freeing memory
     freeaddrinfo(serverinfo);
+    memset(&buffer, 0, sizeof buffer);
 
-    //Receiving
-    if ((nBytes = recv(sockfd, buffer, MAXDATASIZE - 1, 0)) == -1) {
-        perror("Error while receiving");
-        exit(1);
+    printf("Connected to %s\n", s);
+    printf("Enter your message:\n");
+
+    //Loop for keeping connection with the server
+    for(;;) {
+        FD_ZERO(&read_fds);
+        FD_SET(sockfd, &read_fds);
+        FD_SET(0, &read_fds);
+
+        select(sockfd + 1, &read_fds, NULL, NULL, NULL);
+
+        if (FD_ISSET(sockfd, &read_fds)) {
+            memset(&buffer, 0, MAXDATASIZE);
+            read(sockfd, buffer, MAXDATASIZE);
+            printf("Your received: ");
+            printf("%s\n", buffer);
+            printf("Enter your message:\n");
+        } else if (FD_ISSET(0, &read_fds)) {
+            if ((messageLength = read(0, &sendBuffer, MAXDATASIZE - 1)) == -1) {
+                printf("Error while reading input!\n");
+            }
+
+            if (sendBuffer[0] == 'x' && sendBuffer[1] == '/') {
+                break;
+            }
+
+
+            if ((send(sockfd, sendBuffer, messageLength, 0)) == -1) {
+                printf("Error while sending data!\n");
+            }
+
+            memset(&sendBuffer, 0, MAXDATASIZE);
+        }
     }
-
-    buffer[nBytes] = '\0';
-
-    printf("Client: received %s\n", buffer);
 
     close(sockfd);
 
